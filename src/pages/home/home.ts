7324a3a3fc597450f87787a716ba4e485c6e42ed
O Mestre Maximo <omestre@localhost.localdomain>
2017-09-11 21:55:36 -0300

    Exemplo de botõs

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,12 +9,22 @@ export class HomePage {
 
 	public hello: boolean = false;
 
+	//BUTTONS
+	myColor: string = 'dark';
+	isDanger: boolean = true;
+	isRound: boolean = true;
+	isClear: boolean = true;
+
   	constructor(public navCtrl: NavController) {
 
   	}
 
   	showHello(): void {
   		this.hello = !this.hello;
+  	}
+
+  	onClick(event: MouseEvent): void {
+  		console.log(event);
   	}
 
 }
